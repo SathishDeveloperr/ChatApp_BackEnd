321bb1f8fb8b5da6687d6c093c39e383879a53f2
Sathish Pooja <Sathish Pooja@DESKTOP-S39ETOQ>
2025-01-02 17:26:57 +0530

    Client Url Updated

--- index.js
+++ index.js
@@ -27,8 +27,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-
+app.use(cors({
+    origin: 'https://chat-app-frontend-teal-xi.vercel.app', 
+    methods: ['GET', 'POST','PUT','DELETE'],
+    credentials: true,
+}));
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
